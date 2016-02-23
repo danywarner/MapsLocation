9e0cf3883cf4c51e401cd4a41d7398d903f362fa
Daniel Warner <danywarner@gmail.com>
2016-02-23 11:53:39 -0500

    map and location working

---
+++ MapsLocation/LocationVC.swift
@@ -0,0 +1,61 @@
+//
+//  SecondViewController.swift
+//  MapsLocation
+//
+//  Created by Daniel Warner on 2/23/16.
+//  Copyright © 2016 Daniel Warner. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var map: MKMapView!
+
+    let locationManager  = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+      
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+}
+
+
+
+
+
+
